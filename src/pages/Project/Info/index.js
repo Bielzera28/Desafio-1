16d3f216d0f8167e4b50a78d1da2ab231d501d93
Gabriel Aguiar <aguiar.desenvolvedor@gmail.com>
2022-07-22 10:16:32 -0300

    Info e finalização

--- src/pages/Project/Info/index.js
+++ src/pages/Project/Info/index.js
@@ -10,25 +10,25 @@ function Info() {
       <div className="info-home">
         <Home />
       </div>
-      <div>
-        <Text>Produtos</Text>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
+      <div className="info-container__1">
+        <Text fontSize={18} fontWeight={600} color="#272727">Produtos</Text>
+        <Button fontSize={18} fontWeight={400} color="#272727">Mobilia</Button>
+        <Button fontSize={18} fontWeight={400} color="#272727">Segurança</Button>
+        <Button fontSize={18} fontWeight={400} color="#272727">Design Interior</Button>
       </div>
 
-      <div>
-        <Text>Mais Informações</Text>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
+      <div className="info-container__2">
+        <Text fontSize={18} fontWeight={600} color="#272727">Mais Informações</Text>
+        <Button fontSize={18} fontWeight={400} color="#272727">Contrato</Button>
+        <Button fontSize={18} fontWeight={400} color="#272727">Sobre nós</Button>
+        <Button fontSize={18} fontWeight={400} color="#272727">Termos e Condições</Button>
       </div>
 
-      <div>
-        <Text>Redes Sociais</Text>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
+      <div className="info-container__3">
+        <Text fontSize={18} fontWeight={600} color="#272727">Redes Sociais</Text>
+        <Button fontSize={18} fontWeight={400} color="#272727">Pinterest</Button>
+        <Button fontSize={18} fontWeight={400} color="#272727">Facebook</Button>
+        <Button fontSize={18} fontWeight={400} color="#272727">Instagram</Button>
       </div>
     </div>
   );
